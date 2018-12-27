a8b7942a02ca2dcd829f9a5d67f9d11b6cf655b6
s777610 <s777610@gmail.com>
2018-12-27 06:07:34 -0800

    add Post schema for mongodb

--- server.js
+++ server.js
@@ -31,11 +31,10 @@ app.use(passport.initialize());
 // Passport Config
 require("./config/passport")(passport);
 
-// Use Routes
+// Use Routes, those routers are middleware
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
