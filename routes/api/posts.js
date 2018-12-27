e8064f22f30cddc7986d56a10320cfe5fd20816a
s777610 <s777610@gmail.com>
2018-12-27 08:00:40 -0800

    like or unlike post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -82,4 +82,86 @@ router.delete(
   }
 );
 
+// @route   POST api/posts/like/:id
+// @desc    Like post
+// @access  Private
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (
+          post.likes.filter(like => like.user.toString() === req.user.id)
+            .length > 0
+        ) {
+          return res
+            .status(400)
+            .json({ alreadyliked: "User already liked this post" });
+        }
+        // add user id to likes array
+        post.likes.unshift({ user: req.user.id });
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+  }
+);
+
+// @route   POST api/posts/unlike/:id
+// @desc    Like post
+// @access  Private
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (
+          post.likes.filter(like => like.user.toString() === req.user.id)
+            .length === 0
+        ) {
+          return res
+            .status(400)
+            .json({ notliked: "You have not yet liked this post" });
+        }
+        // Get remove index
+        const removeIndex = post.likes
+          .map(item => item.user.toString())
+          .indexOf(req.user.id);
+
+        // Splice out of array
+        post.likes.splice(removeIndex, 1);
+
+        // Save
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No post found" }));
+  }
+);
+
+/*
+// @route   POST api/posts/like/:id
+// @desc    Like or Unlike post
+// @access  Private
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    let post = await Post.findById(req.params.id);
+    if (post == null)
+      return res.status(404).json({ postnotfound: "No post found" });
+    const index = post.likes.findIndex(l => {
+      return l.user == req.user.id;
+    });
+    if (index === -1) {
+      post.likes.push({ user: req.user.id });
+    } else {
+      post.likes.splice(index, 1);
+    }
+    post = await post.save();
+    res.json(post);
+  }
+);
+*/
+
 module.exports = router;
