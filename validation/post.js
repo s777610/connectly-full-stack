118027b8db15ff10bc1823cf1fbb7cad508239e2
s777610 <s777610@gmail.com>
2018-12-27 06:57:59 -0800

    be able to delete post by id

---
+++ validation/post.js
@@ -0,0 +1,21 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  data.text = !isEmpty(data.text) ? data.text : "";
+
+  if (!Validator.isLength(data.text, { min: 10, max: 350 })) {
+    errors.text = "Post must be betweeb 10 and 350 characters";
+  }
+
+  if (Validator.isEmpty(data.text)) {
+    errors.text = "Text field is required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
