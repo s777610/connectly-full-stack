7849c30a646e9e752094bcf71b9568f57158f737
s777610 <s777610@gmail.com>
2018-12-31 12:25:43 -0800

    add profileReduer and dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,15 @@ import { BrowserRouter, Route } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 import { Provider } from "react-redux";
 import store from "./store";
-
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
-
+import Dashboard from "./components/dashboard/Dashboard";
 import "./App.css";
 
 // Check for if token in localStorage
@@ -28,7 +28,8 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     // Logout user
     store.dispatch(logoutUser());
-    // TODO: Clear current Profile
+    // Clear current Profile
+    store.dispatch(clearCurrentProfile());
 
     // Redirect to login
     window.location.href = "/login";
@@ -46,6 +47,7 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <Route exact path="/dashboard" component={Dashboard} />
             </div>
             <Footer />
           </div>
