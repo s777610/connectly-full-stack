711edbda5200036ff2a7dfdd648049cc283ebef7
s777610 <s777610@gmail.com>
2018-12-31 15:40:33 -0800

    add createProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,7 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/create-profile/CreateProfile";
 import "./App.css";
 
 // Check for if token in localStorage
@@ -50,8 +51,16 @@ class App extends Component {
             <div className="container">
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute
+                  exact
+                  path="/create-profile"
+                  component={CreateProfile}
+                />
               </Switch>
             </div>
             <Footer />
