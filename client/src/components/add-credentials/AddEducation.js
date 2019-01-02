aec339df14b5a990bbe41d1116a660747cb85963
s777610 <s777610@gmail.com>
2019-01-02 02:07:58 -0800

    add Education and Experience and delete education and experiecne action creator

---
+++ client/src/components/add-credentials/AddEducation.js
@@ -0,0 +1,151 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import TextFieldGroup from "../common/TextFieldGroup";
+import TextAreaFieldGroup from "../common/TextAreaFieldGroup";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addEducation } from "../../actions/profileActions";
+import { clearError } from "../../actions/clearErrorAction";
+
+class AddEducation extends Component {
+  state = {
+    school: "",
+    degree: "",
+    fieldofstudy: "",
+    from: "",
+    to: "",
+    current: false,
+    description: "",
+    disabled: false
+  };
+
+  componentWillUnmount() {
+    this.props.clearError();
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+
+    const eduData = {
+      ...this.state
+    };
+    this.props.addEducation(eduData, this.props.history);
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  onCheck = event => {
+    this.setState({
+      disabled: !this.state.disabled,
+      current: !this.state.current
+    });
+  };
+
+  render() {
+    const errors = this.props.errors;
+    return (
+      <div className="add-education">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <Link to="/dashboard" className="btn btn-light">
+                Go Back
+              </Link>
+              <h1 className="display-4 text-center">Add Education</h1>
+              <p className="lead text-center">
+                Add any school, bootcamp, etc that you have attended
+              </p>
+              <small className="d-block pb-3">* = required fields</small>
+              <form onSubmit={this.onSubmit}>
+                <TextFieldGroup
+                  placeholder="* School"
+                  name="school"
+                  value={this.state.school}
+                  onChange={this.onChange}
+                  error={errors.school}
+                />
+                <TextFieldGroup
+                  placeholder="* Degree"
+                  name="degree"
+                  value={this.state.degree}
+                  onChange={this.onChange}
+                  error={errors.degree}
+                />
+                <TextFieldGroup
+                  placeholder="* Field of Study"
+                  name="fieldofstudy"
+                  value={this.state.fieldofstudy}
+                  onChange={this.onChange}
+                  error={errors.fieldofstudy}
+                />
+                <h6>From Date</h6>
+                <TextFieldGroup
+                  name="from"
+                  type="date"
+                  value={this.state.from}
+                  onChange={this.onChange}
+                  error={errors.from}
+                />
+                <h6>To Date</h6>
+                <TextFieldGroup
+                  name="to"
+                  type="date"
+                  value={this.state.to}
+                  onChange={this.onChange}
+                  error={errors.to}
+                  disabled={this.state.disabled ? "disabled" : ""}
+                />
+                <div className="form-check mb-4">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    name="current"
+                    value={this.state.current}
+                    checked={this.state.current}
+                    // onCheck will change disabled and current
+                    onChange={this.onCheck}
+                    id="current"
+                  />
+                  <label htmlFor="current" className="form-check-label">
+                    Current Status
+                  </label>
+                </div>
+                <TextAreaFieldGroup
+                  placeholder="Program Description"
+                  name="description"
+                  value={this.state.description}
+                  onChange={this.onChange}
+                  error={errors.description}
+                  info="The program that you were in"
+                />
+                <input
+                  type="submit"
+                  value="Submit"
+                  className="btn btn-info btn-block mt-4"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+AddEducation.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
+  addEducation: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { addEducation, clearError }
+)(withRouter(AddEducation));
