7ed941aa4a7a46eee7408ffbb934764233fdeb47
s777610 <s777610@gmail.com>
2019-01-04 20:24:27 -0800

    add profile header and profile about

---
+++ client/src/components/profile/ProfileGithub.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ProfileGithub extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>TODO: PROFILE Github</h1>
+      </div>
+    );
+  }
+}
+
+export default ProfileGithub;
