52d208cf8f3f9eb1377bfbe4d633048ac5f03224
s777610 <s777610@gmail.com>
2019-01-29 19:07:10 -0800

    refactor passport.js

--- config/passport.js
+++ config/passport.js
@@ -4,25 +4,29 @@ const mongoose = require("mongoose");
 const User = mongoose.model("users");
 const keys = require("../config/keys");
 
-const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = keys.secretOrKey;
+// Setup options for JWT Strategy
+const opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: keys.secretOrKey
+};
 
+// Create JWT strategy, done is callback func
 // passport extract jwt from incoming request
 // and get jwt_payload from jwt
 // find user form db
 // add user to req obj as req.user
-module.exports = passport => {
-  passport.use(
-    new JwtStrategy(opts, (jwt_payload, done) => {
-      User.findById(jwt_payload.id)
-        .then(user => {
-          if (user) {
-            return done(null, user);
-          }
-          return done(null, false);
-        })
-        .catch(err => console.log(err));
+const authStrategy = new JwtStrategy(opts, (jwt_payload, done) => {
+  User.findById(jwt_payload.id)
+    .then(user => {
+      if (user) {
+        return done(null, user);
+      }
+      return done(null, false);
     })
-  );
+    .catch(err => console.log(err));
+});
+
+// Tell passport to use this strategy
+module.exports = passport => {
+  passport.use(authStrategy);
 };
